a7d6c47ac8ee6251b0741d99dc203f7fff06d3a6
Russell Simmons <rws10@live.com>
2017-07-10 16:46:07 -0400

    Initial commit. Began adding the models.

---
+++ Carrol Lawn Care/Carrol Lawn Care/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Carrol_Lawn_Care.MvcApplication" Language="C#" %>
---
+++ Carrol Lawn Care/Carrol Lawn Care/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Carrol_Lawn_Care.Startup))]
+namespace Carrol_Lawn_Care
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
