be288aeef2233111adf676d7b39ad4ab2b5db37d
Russell Simmons <rws10@live.com>
2017-07-14 15:50:59 -0400

    Added Employee and Person models

---
+++ Carrol Lawn Care/Carrol Lawn Care/Models/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Carrol_Lawn_Care.Models
+{
+    public class Employee
+    {
+        [Key]
+        [Required]
+        public int empid { get; set;}
+
+        // the parent object (person) of this employee
+        [ForeignKey("Person")]
+        [Required]
+        public int perid { get; set;}
+
+        public virtual Person person { get; set;}
+
+        // how much the employee is paid
+        [Required(ErrorMessage = "Name Required")]
+        public double payRate { get; set;}
+
+        // the employee's social security number
+        public int ssn { get; set;}
+
+        // if the employee is not a manager, who is their manager?
+        public int managedby { get; set; }
+
+        // if the employee is a manager, who do they manage?
+        public int manages { get; set; }
+
+    }
+}
---
+++ Carrol Lawn Care/Carrol Lawn Care/Models/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Carrol_Lawn_Care.Models
+{
+    public class Person
+    {
+        
+        [Key]
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int perId { get; set;}
+
+        [Required(ErrorMessage = "Name Required")]
+        [Display(Name = "Name")]
+        public string name { get; set; }
+
+        [Display(Name = "Name")]
+        public string phone { get; set; }
+
+        [Display(Name = "Address")]
+        public string address { get; set; }
+    }
+}
